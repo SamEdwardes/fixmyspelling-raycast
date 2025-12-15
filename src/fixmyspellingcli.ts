c00f071756fe77b99aed4455b20e1f0a7a21c830
SamEdwardes <edwardes.s@gmail.com>
2025-12-15 11:35:10 -0800

    fixmyspellingcli now working

--- src/fixmyspellingcli.ts
+++ src/fixmyspellingcli.ts
@@ -8,33 +8,17 @@ const execAsync = promisify(exec);
 
 export default async function main() {
 
-  const bun = path.join(
-    os.homedir(),
-    '.bun',
-    'bin',
-    'bun',
-  )
-
-  const script = path.join(
-    os.homedir(),
-    'projects',
-    'personal',
-    'fixmyspelling',
-    'src',
-    'index.ts'
-  )
-
   const fixmyspelling = path.join(
     os.homedir(),
     ".local",
     "bin",
-    'fixmyspelling',
+    'fixmyspelling'
   )
 
   try {
     const selectedText = await getSelectedText();
-    // const { stdout, stderr } = await execAsync([fixmyspelling, `'${selectedText}'`].join(' '));
-    const { stdout, stderr } = await execAsync([bun, script, `'${selectedText}'`].join(' '));
+    const command = [fixmyspelling, `'${selectedText}'`].join(' ')
+    const { stdout, stderr } = await execAsync(command);
     await showHUD("Spelling fixed")
     Clipboard.paste(stdout)
   } catch (error) {
